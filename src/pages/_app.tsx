4290d1327ff99f48904477fc9bd831690401c079
gseganzerla <g.seganzerla@gmail.com>
2022-04-28 22:34:32 -0300

    mirage setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import { theme } from '../styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
+import { makeServer } from '../services/mirage'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/services/mirage.ts
@@ -0,0 +1,41 @@
+import { ActiveModelSerializer, createServer, Factory, Model } from 'miragejs'
+import { Task } from './task'
+import faker from '@faker-js/faker'
+
+export function makeServer() {
+  const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+    models: {
+      task: Model.extend<Partial<Task>>({}),
+    },
+    factories: {
+      task: Factory.extend({
+        title() {
+          return faker.word.preposition()
+        },
+        isFinished() {
+          return faker.datatype.boolean()
+        },
+        createdAt() {
+          return faker.date.recent(5)
+        },
+      }),
+    },
+    seeds(server) {
+      server.createList('task', 5)
+    },
+    routes() {
+      this.namespace = 'api'
+      this.timing = 300
+
+      this.get('tasks')
+
+      this.namespace = ''
+      this.passthrough()
+    },
+  })
+
+  return server
+}
